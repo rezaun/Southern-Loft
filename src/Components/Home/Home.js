1c8caadf2497b42bd21e72b45a757e38cec7183b
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-07-31 01:49:54 +0600

    home oage image changes and footer background colro chnage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,9 +12,9 @@ const Home = () => {
         <Header />
         <Zoom left>
         <div className='text-center mt-5 up'>
-            <h1 className='text-center text-white headline display-2 pt-5'>Creating Value Globally</h1>
-            <p className='text-white fs-6'>Your Trusted Furniture Partner</p>
-            <h4 className='headline text-warning'>MAHIRA FURNITURE</h4>
+            <h1 className='text-center text-black headline display-2 pt-5'>Creating Value Globally</h1>
+            <p className='text-black fs-6'>Your Trusted Furniture Partner</p>
+            <h4 className='headline text-black'>Southern Loft </h4>
             <button class="btn btn-danger mt-4 mb-3">Contact Us</button>
             
         </div>
